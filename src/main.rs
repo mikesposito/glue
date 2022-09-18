31af159cfa0463d29d0872343803187c9ac6dabf
Michele Esposito <michele.esposito@consensys.net>
2022-09-19 01:56:01 +0200

    feat: add file execution

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 mod args;
 pub mod glue;
 
-use args::{parse_command_args, Args};
+use args::{parse_command_args, Args, print_help};
 use glue::GlueStack;
 
 #[tokio::main]
 async fn main() {
 	let args: Args = parse_command_args();
 
-	let mut glue_stack = match GlueStack::from_string(&args.request) {
-		Err(x) => panic!("Error encountered while creating glue: {}", x),
-		Ok(x) => x,
+	if args.file.is_none() && args.request.is_none() {
+		print_help();
+		return;
+	}
+
+	let mut glue_stack = match args.file {
+		None => match GlueStack::from_string(&args.request.unwrap()) {
+			Err(x) => panic!("Error encountered while creating glue: {}", x),
+			Ok(x) => x,
+		},
+		Some(x) => match GlueStack::from_file(&x) {
+			Err(x) => panic!("Error encountered while creating glue: {}", x),
+			Ok(x) => x,
+		}
 	};
 
 	match glue_stack.execute().await {

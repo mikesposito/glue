a55fba9c2bd5bd82437870139a926dbb8243be46
Michele Esposito <michele.esposito@consensys.net>
2022-09-20 02:36:12 +0200

    chore: uninstall rust after install if wasn't installed before

--- install.sh
+++ install.sh
@@ -1,35 +1,58 @@
 #!/usr/bin/env bash
 
-echo "Welcome to the glue installer. The current version of glue will be installed"
+echo '           $$                      '
+echo '           $$ |                     '
+echo '  $$$$$$   $$ |$$    $$  $$$$$$   '
+echo ' $$  __$$  $$ |$$ |  $$ |$$  __$$  '
+echo ' $$ /  $$ |$$ |$$ |  $$ |$$$$$$$$ | '
+echo ' $$ |  $$ |$$ |$$ |  $$ |$$   ____| '
+echo '  $$$$$$$ |$$ | $$$$$$  | $$$$$$$   '
+echo '   ____$$ | __|  ______/  \_______| '
+echo ' $$    $$ |                         '
+echo '  $$$$$$  |                         '
+echo '   ______/                          '
+printf "\n\n"
+printf "Welcome to the glue installer. The current version of glue will be installed\n\n"
 
 # Install Rust
+NEW_CARGO=false
 if ! command -v cargo --version &> /dev/null
 then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | RUSTUP_INIT_SKIP_PATH_CHECK=true sh -s -- -y
+  NEW_CARGO=true
 fi
 
 # Build glue
 cargo build --release
 
-# Move to Glue home
+# Create Glue Home
 mkdir $HOME/.glue
 mkdir $HOME/.glue/bin
+export GLUE_HOME=$HOME/.glue
 
-cp target/release/glue $HOME/.glue/bin
+# Move executable to Glue Home
+cp target/release/glue $GLUE_HOME/bin
+chmod +x $GLUE_HOME/bin/glue
 
-if [[ ":$PATH:" == *"$HOME/.glue/bin"* ]]; then
-  echo "Your path is correctly set already."
+if [[ ":$PATH:" == *"$GLUE_HOME/bin"* ]]; then
+  printf "$GLUE_HOME/bin is correctly set to your PATH already.\n"
 else
   if [[ "$SHELL" == *"zsh" ]]; then
-    echo "export PATH=$PATH:$HOME/.glue/bin" >> $HOME/.zshrc
+    echo "export PATH=$PATH:$GLUE_HOME/bin" >> $HOME/.zshrc
   fi
 
   if [ -n "$BASH_VERSION" ]; then
     echo 'export PATH=$PATH:$HOME/.glue/bin' >> $HOME/.bashrc
   fi
 
-  echo "$HOME/.glue/bin Added to your path."
+  printf "$GLUE_HOME/bin Added to your PATH.\n"
 fi
 
-echo "Install completed. Restart your terminal window or open a new one to execute the glue command"
+# if Rust was not previously installed, uninstall it
+if $NEW_CARGO; then
+  rustup self uninstall -y
+fi
+
 $HOME/.glue/bin/glue --version
+
+printf "\nInstall completed. Restart your terminal window or open a new one to execute the glue command\n"

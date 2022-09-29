c14afe759fd32826c06bb4dcf859aacd737d91c4
Michele Esposito <michele.esposito@consensys.net>
2022-09-30 00:54:56 +0200

    chore: use cargo install in installer

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ echo ' $$    $$ |                         '
 echo '  $$$$$$  |                         '
 echo '   ______/                          '
 printf "\n\n"
-printf "Welcome to the glue installer. The current version of glue will be installed\n\n"
 
 # Install Rust
 NEW_CARGO=false
@@ -23,45 +22,11 @@ then
 fi
 
 # Build glue
-cargo build --release
-
-# Create Glue Home
-mkdir $HOME/.glue
-mkdir $HOME/.glue/bin
-export GLUE_HOME=$HOME/.glue
-
-# Move older version if exists
-if [ -f "$GLUE_HOME/bin/glue" ]; then
-  mv $GLUE_HOME/bin/glue $GLUE_HOME/bin/glue.old
-fi
-
-# Move executable to Glue Home
-cp target/release/glue $GLUE_HOME/bin
-chmod +x $GLUE_HOME/bin/glue
-
-if [[ ":$PATH:" == *"$GLUE_HOME/bin"* ]]; then
-  printf "$GLUE_HOME/bin is correctly set to your PATH already.\n"
-else
-  if [[ "$SHELL" == *"zsh" ]]; then
-    echo "export PATH=$PATH:$GLUE_HOME/bin" >> $HOME/.zshrc
-  fi
-
-  if [ -n "$BASH_VERSION" ]; then
-    echo 'export PATH=$PATH:$HOME/.glue/bin' >> $HOME/.bashrc
-  fi
-
-  printf "$GLUE_HOME/bin Added to your PATH.\n"
-fi
+cargo install --path .
 
 # if Rust was not previously installed, uninstall it
 if $NEW_CARGO; then
   rustup self uninstall -y
 fi
-
-if [ -f "$GLUE_HOME/bin/glue.old" ]; then
-  rm $GLUE_HOME/bin/glue.old
-fi
-
-$HOME/.glue/bin/glue --version
 
 printf "\nInstall completed. Restart your terminal window or open a new one to execute the glue command\n"

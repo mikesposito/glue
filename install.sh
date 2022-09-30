859a2674b3fdf85d12d362de6874161339707b0f
Michele Esposito <michele.esposito@consensys.net>
2022-09-30 02:37:44 +0200

    chore: installer dont uninstall cargo

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ echo '   ______/                          '
 printf "\n\n"
 
 # Install Rust
-NEW_CARGO=false
 if ! command -v cargo --version &> /dev/null
 then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | RUSTUP_INIT_SKIP_PATH_CHECK=true sh -s -- -y
@@ -23,10 +22,5 @@ fi
 
 # Build glue
 cargo install --path .
-
-# if Rust was not previously installed, uninstall it
-if $NEW_CARGO; then
-  rustup self uninstall -y
-fi
 
 printf "\nInstall completed. Restart your terminal window or open a new one to execute the glue command\n"

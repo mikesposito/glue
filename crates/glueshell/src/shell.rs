a04dee4b311ef9724c0ec92ba8a9732781ae70b7
Michele Esposito <michele.esposito@consensys.net>
2022-10-07 19:07:56 +0200

    refactor: add glueshell struct

---
+++ crates/glueshell/src/shell.rs
@@ -0,0 +1,52 @@
+use crate::utils::{print_err, prompt};
+use gluerunner::Stack;
+
+/// An interactive shell that runs glue commands using a stack.
+/// Commands are incrementally added and executed as they are passed
+/// to cin from prompt.
+pub struct Shell {
+  /// The `Stack` instance of the shell.
+  /// This instance lives for the entire `Shell` instance lifetime.
+	stack: Stack,
+}
+
+impl Shell {
+  /// Creates a new `Shell` instance with an empty `Stack`.
+	pub fn new() -> Self {
+		Shell {
+			stack: Stack::new(),
+		}
+	}
+
+  /// Start the prompt loop asynchronously. The loop executes a glue command
+  /// at each iteration, or ends when a `None` is provided as command. 
+  /// 
+  /// Note that a `None` command equals to a prompt with a `exit` or `quit`
+  /// string: empty string will just be ignored and the loop will continue.
+	pub async fn start(self: &mut Self) -> () {
+		loop {
+      // Get next command from prompt. Break loop if None.
+			let glue_command = match prompt() {
+				None => break,
+				Some(x) => x,
+			};
+
+			if glue_command != "" {
+        // Add command to stack, print error and skip loop iteration on error.
+				match self.stack.push_runner_from_string(&glue_command, false) {
+					Err(x) => {
+						print_err(x);
+						return;
+					}
+					_ => (),
+				};
+
+        // Execute the command and print result.
+				match self.stack.execute_next().await {
+					Err(x) => print_err(x),
+					Ok(_) => println!("{}", self.stack.current().unwrap().result.as_ref().unwrap()),
+				};
+			}
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,42 @@ mod args;
 
 use args::{command_args, Args};
 use gluerunner::{heap, Runner};
+use glueshell::Shell;
 
 #[tokio::main]
 async fn main() {
 	let args: Args = command_args();
 
 	if args.file.is_none() && args.request.is_none() {
-		glueshell::interactive().await;
+		// If no file and no request are provided, a new `Shell`
+		// is instantiated to prompt command in loop.
+		let mut shell = Shell::new();
+
+		// Start the shell and block till finished.
+		shell.start().await;
 		return;
 	}
 
+	// Instantiate an `HashMap` protected by a `Mutex` to allow
+	// request nodes to share data between each other from
+	// different non-blocking threads
 	let heap = heap();
 
 	let mut glue_runner = match args.file {
+		// Create `Runner` from command string if no file is provided
 		None => match Runner::from_string(&args.request.unwrap(), heap, args.verbose) {
 			Err(x) => panic!("Error encountered while creating glue: {}", x),
 			Ok(x) => x,
 		},
+
+		// Or use a file content
 		Some(x) => match Runner::from_file(&x, heap, args.verbose) {
 			Err(x) => panic!("Error encountered while creating glue: {}", x),
 			Ok(x) => x,
 		},
 	};
 
+	// Execute command and print result. Panic on error.
 	match glue_runner.execute().await {
 		Err(x) => panic!("Error encountered while executing requests: {}", x),
 		Ok(_) => println!("{}", glue_runner.result.unwrap()),
